a9f78da6f9715bd20126885fd8ca41263aa1ca78
Kevin Carrig <kevincarrig1@gmail.com>
2021-03-14 15:53:02 -0400

    New description

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,6 +4,42 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+ $(document).ready(function() {
+
+     $('.inputAnimate').each(function() {
+
+         var self = $(this);
+         var input = self.children('input');
+         var span = $('<span />').prependTo(self);
+         var div = $('<div />').appendTo(self);
+         var em = $('<em />').appendTo(self);
+
+         input.keypress(function (e) {
+             if(e.which && e.charCode) {
+                 resizeForText(self, span, $(this).val() + String.fromCharCode(e.keyCode | e.charCode));
+             }
+         });
+
+         input.keyup(function(e) {
+             if(e.keyCode === 8 || e.keyCode === 46) {
+                 resizeForText(self, span, $(this).val());
+             }
+         });
+
+         resizeForText(self, span, input.val());
+
+     });
+
+ });
+
+ function resizeForText(self, span, text) {
+     text = (!text) ? ' ' : text;
+     span.text(text);
+     self.css('--offsetLeft', span.width() + 3);
+     self.css('--offsetLeftScale', span.width() + 19);
+ }
+
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
